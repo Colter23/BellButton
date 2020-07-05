e6d8b18a477366592c10e3189f8facbba08d3e32
Colter <13864833022@163.com>
2020-07-06 00:28:44 +0800

    稍稍更新一下相册

--- src/components/album/AlbumItem.vue
+++ src/components/album/AlbumItem.vue
@@ -1,13 +1,53 @@
 <template>
-	
+	<div class="img-item" @mouseenter="showAuthor=true" @mouseleave="showAuthor=false">
+		<img @load="updated" :src="imgSrc" style="width: 100%;" alt="">
+		<span class="author" v-show="showAuthor">
+			<a :href="authorLink" style="color: #fff" target="_blank">B站 {{imgInfo.author}}</a>
+		</span>
+	</div>
 </template>
 
 <script>
 	export default {
-		name: "AlbumItem"
+		name: "AlbumItem",
+		props: {
+			imgInfo: {},
+		},
+		data: ()=>({
+			showAuthor: false
+		}),
+		methods:{
+			updated(){
+				this.$emit('imgload')
+			}
+		},
+		computed: {
+			imgSrc(){
+				return "http://img.mczhengyi.top/BellButton/img/album/" + this.imgInfo.path + "!web"
+			},
+			authorLink(){
+				return "https://space.bilibili.com/" + this.imgInfo.BID
+			}
+		}
 	}
 </script>
 
 <style scoped>
+	.img-item{
+		width: 100%;
+		/*height: 100%;*/
+	}
 
+	.author{
+		display: block;
+		position: absolute;
+		bottom: 5px;
+		right: 10px;
+		height: 50px;
+		width: 100%;
+		line-height: 50px;
+		background: linear-gradient(rgba(0,0,0,0.0), rgba(0,0,0,0.4));;
+		color: #ffffff;
+		font-size: 20px;
+	}
 </style>
--- src/views/bellalbum/BellAlbum.vue
+++ src/views/bellalbum/BellAlbum.vue
@@ -3,9 +3,8 @@
 		<div style="margin-bottom: 20px">Bell相册  未完成  敬请期待prpr</div>
 
 		<div class="grid" >
-			<div class="grid-item" v-for="(item, index) in imges" @mouseenter="showAuthor=true" @mouseleave="showAuthor=false">
-				<img @load="updated" :src="imgSrc(item.path)" style="width: 100%;" alt="">
-				<span class="author" v-show="showAuthor">{{item.author}}</span>
+			<div class="grid-item" v-for="(item, index) in imges" :key="index">
+				<album-item @imgload="updated" :imgInfo="item"></album-item>
 			</div>
 		</div>
 
@@ -13,18 +12,18 @@
 </template>
 
 <script>
+
 	import imges from "assets/imges.json";
 	import Masonry from "masonry-layout";
+	import AlbumItem from "components/album/AlbumItem";
+
 	export default {
 		name: "BellAlbum",
+		components: {AlbumItem},
 		data: () => ({
 			imges: imges.groups[0].imglist,
-			showAuthor: false
 		}),
 		methods: {
-			imgSrc(src){
-				return "http://img.mczhengyi.top/BellButton/img/album/" + src + "!web";
-			},
 			updated() {
 				var grid = document.querySelector(".grid");
 				var msnry = new Masonry(grid, {
@@ -46,11 +45,6 @@
 		overflow: hidden;
 		width: 90%;
 	}
-	.img-item img{
-		display: block;
-		width: 100%;
-		height: auto;
-	}
 	.grid-item {
 		position: relative;
 		padding: 10px 10px 0;
@@ -58,11 +52,4 @@
 		width: var(--img-width);
 	}
 
-	.author{
-		position: absolute;
-		bottom: 5px;
-		right: 10px;
-		color: #75bbea;
-		font-size: 20px;
-	}
 </style>
